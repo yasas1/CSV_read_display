cafa916a2a9909bf6c1eaf1422037260f07edf0b
yasas1 <32437531+yasas1@users.noreply.github.com>
2018-08-21 10:42:11 +0530

    updated

--- CSVread_display/frontend/src/app/home/home.component.ts
+++ CSVread_display/frontend/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import * as Chart from 'chart.js';
+import * as Chart from 'chart.js';
 //import  { CsvserveService } from '../csvserve.service';
 
 
@@ -13,26 +13,27 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-   // this.chart();
-  }/*
+    this.chart();
+  }
+
   chart(){
-  new Chart(document.getElementById("pie-chart"), {
-    type: 'pie',
-    data: {
-      labels: ["Africa", "Asia", "Europe"],
-      datasets: [{
-        label: "Population (millions)",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [2478,5267,734]
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
+    new Chart(document.getElementById("pie-chart"), {
+      type: 'pie',
+      data: {
+        labels: ["Africa", "Asia", "Europe"],
+        datasets: [{
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734]
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Predicted world population (millions) in 2050'
+        }
       }
-    }
-});
-  }*/
+    });
+  }
 
 }
